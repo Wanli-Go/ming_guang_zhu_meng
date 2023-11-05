9480b95a3dcdeb0946de0f74822aed57af1cef09
puppytag <125252979+puppytag@users.noreply.github.com>
2023-11-05 15:32:48 +0800

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/material.dart';
-import './volunteer/my_app.dart'; // 调整这个路径来决定你的 App 的调试的 Page
+// import 'child/my_favourite.dart'; // 调整这个路径来决定你的 App 的调试的 Page
+// import 'child/compare.dart';
+// import 'donor/donation_record.dart';
+// import 'donor/share.dart';
+// import 'child/points_mall.dart';
+// import 'child/search.dart';
+import 'child/points_mall.dart';
 
 void main() {
-  runApp(MaterialApp(
-    theme: ThemeData(
-      fontFamily: "StarRail", // 定义 App 的字体
-      iconTheme: const IconThemeData(
-          // 定义 图标 的颜色
-          color: Colors.indigo),
-    ),
 
-    home: const RankPage(), // Your widget
-  ));
+  runApp(
+
+    MaterialApp(
+
+      theme: ThemeData(
+        fontFamily: "StarRail", // 定义 App 的字体
+        iconTheme: const IconThemeData( // 定义 图标 的颜色
+          color: Colors.indigo
+        ),
+      ),
+
+      home: const MyApp(), // Your widget
+
+    )
+
+  );
 }
